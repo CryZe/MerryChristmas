524aa833ced7214cdaca03919e71f0c5499f9cd6
Christopher Serr <cryze92@gmail.com>
2014-12-24 16:03:04 +0100

    Branched off German, so this is English now

--- MerryChristmas/Program.cs
+++ MerryChristmas/Program.cs
@@ -6,8 +6,8 @@ namespace MerryChristmas
     {
         static void Main(string[] args)
         {
-            var textSpeaker = new TextSpeaker(new CultureInfo("de-De"));
-            var text = "Frohe Weihnachten \{args.Length > 0 ? args[0] : ""}";
+            var textSpeaker = new TextSpeaker(new CultureInfo("en-Us"));
+            var text = "Merry Christmas \{args.Length > 0 ? args[0] : ""}";
             textSpeaker.Speak(text);
             var player = new SilentNightPlayer(Midi.Instrument.AcousticGrandPiano);
             using (player)
